3a810ab8178f32d27db829e0da9dbb62940b5700
Li Site <8lisite@tongji.edu.cn>
2017-03-24 15:54:07 +0800

    meet some problems, cancelled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+
 
 @NgModule({
   declarations: [
@@ -25,6 +27,6 @@ import { TabsPage } from '../pages/tabs/tabs';
     HomePage,
     TabsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, TextToSpeech]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,82 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { Platform } from 'ionic-angular';
+
+declare var SpeechRecognition: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
 
   myIcon: string = "ios-mic";
 
+  text: string;
+
+  constructor(public platform: Platform, private tts: TextToSpeech, public navCtrl: NavController) {
+
+  }
+
+  sayText=function(){
+    try {
+      this.tts.speak(this.text);
+      console.log("successfully spoke" + this.text);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  recognition: any;
+
+  recognizedText: string = "";
+
+  SpeechToText=function (){
+    alert(123);
+  }
+
+  open_win = function() {
+    window.open("https://www.baidu.com/s?wd=" + this.recognizedText);
+    alert(this.data.speechText);
+  };
+
 }
+
+
+
+// .controller('AppCtrl', function($scope) {
+//   //Text to Speech
+//   $scope.data = {
+//     speechText: ''
+//   };
+//   $scope.speakText = function() {
+//     TTS.speak({
+//            text: $scope.data.speechText,
+//            locale: 'en-GB',
+//            rate: 1.5
+//        }, function () {
+//            // Do Something after success
+//        }, function (reason) {
+//            // Handle the error case
+//        });
+//   };
+//   //Speech to Text
+//   $scope.recognizedText = '';
+//   $scope.record = function() {
+//     var recognition = new SpeechRecognition();
+//     recognition.onresult = function(event) {
+//         if (event.results.length > 0) {
+//             $scope.recognizedText = event.results[0][0].transcript;
+//             $scope.$apply()
+//         }
+//     };
+//     recognition.start();
+//   };
+
+//   $scope.open_win = function() {
+//       window.open("https://www.baidu.com/s?wd=" + $scope.recognizedText);
+//   };
+// });
